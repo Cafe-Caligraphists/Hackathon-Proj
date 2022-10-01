59ece1f3fc343990d4b08f11b89f24a7b5bd0df3
Gabrielle King <cafecaligraphists@gmail.com>
2022-10-01 11:36:37 -0500

    added second input section for spending

--- sase-hackathon/src/App.js
+++ sase-hackathon/src/App.js
@@ -3,15 +3,33 @@ import './App.css';
 function App() {
   return (
     <div className="bg">
+
       <header>Program by the Cafe Calligraphists</header>
       <h3 className="title">Money Mapper</h3>
+
       <body>
+
         <div className="input-space">
-          <label className="input-lbl" for="userMoney">Current Balance: </label>
-          <input className="inputs" placeholder="i.e. 8000.25" type="text" id="userMoney" name="userMoney"/>
+
+          <label className="input-lbl" for="user-money">Current Balance: </label>
+          <input className="inputs" placeholder="i.e. 8000.25" type="text" id="user-money" name="user-money"/>
               &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
               &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-          <input className="button" type="submit" value="Submit"></input>
+          <input className="button" type="submit" id="money-submit" value="Submit"></input>
+
+        </div>
+
+        <br/><br/><br/>
+
+        <div className="input-space">
+
+          <label className="input-lbl" for="user-spent">Amount Spent: </label> 
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input className="inputs" placeholder="i.e. 8000.25" type="text" id="user-spent" name="user-spent"/>
+              &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+              &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+          <input className="button" type="submit" id="spent-submit" value="Submit"></input>
+
         </div>
       </body>
     </div>
