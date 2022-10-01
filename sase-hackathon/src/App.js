049a9a7250502573d25a79b44e8fc20fb1aebe88
Gabrielle King <cafecaligraphists@gmail.com>
2022-10-01 10:57:26 -0500

    adding and styling inputs

--- sase-hackathon/src/App.js
+++ sase-hackathon/src/App.js
@@ -6,7 +6,11 @@ function App() {
       <header>Program by the Cafe Calligraphists</header>
       <h3 className="title">Money Mapper</h3>
       <body>
-        <p>Text</p>
+        <div className="input-space">
+          <label className="input-lbl" for="userMoney">Current Balance: </label>
+          <input className="inputs" placeholder="i.e. 8000.25" type="text" id="userMoney" name="userMoney"/><nbsp/>
+          <input className="button" type="submit" value="Submit"></input>
+        </div>
       </body>
     </div>
   );
