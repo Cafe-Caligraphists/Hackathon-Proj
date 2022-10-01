6366a53946b6c407e71a6ef6cd61679061bdd56e
Gabrielle King <cafecaligraphists@gmail.com>
2022-10-01 16:40:42 -0500

    Final Version

--- sase-hackathon/src/App.js
+++ sase-hackathon/src/App.js
@@ -1,37 +1,45 @@
 import './App.css';
-import React from 'react';
-import { Line } from "react-chartjs-2";
+import React, { useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
-function App() {
-
-  var coordinatePairs = [];
+var coordinatePairs = []; //empty array with ordered pairs
   var currentBalance; 
 
-  class newBalance {
+  class newBalance { //constructor
     constructor(moneySpent, time) {
       this.time = time;
       this.moneySpent = moneySpent;
 
     } 
   }
-  function assignBalance() {
-    currentBalance = document.getElementById('user-money').value;
+  function assignBalance() { //gets the current balance from the text box
+    App.currentBalance = document.getElementById('user-money').value;
   }
 
-  function buttonPressed() {
-    //read input somehow
-    var temp = new newBalance(1, document.getElementById('user-spent').value);
-    coordinatePairs.push(temp);
-    var netChange = Math.integral();
+  function buttonPressed() { //gets the money spent 
+    var temp = new newBalance(1, assignBalance.currentbalance - document.getElementById('user-spent').value); //creates ordered pairs
+    coordinatePairs.push(temp); 
+    var netChange = Math.integral(); 
+    currentBalance -= document.getElementById('user-spent').value;
 
   }
 
-  function sum(endbound) {
+  function sum(endbound) { //adds all the values in the array except for the first and last ones
     for (let i = 1; i < endbound; i++) {
       var sum = 0;
       sum += coordinatePairs[i].moneySpent;
     }
-    sum = ((Math.integral.g)/2) * ((sum * 2) + coordinatePairs[0] + coordinatePairs[endbound]);
+    sum = ((Math.integral.g)/2) * ((sum * 2) + coordinatePairs[0] + coordinatePairs[endbound]); //reimann sums
   }
 
   Math.integral=function(x, y)  
@@ -39,8 +47,7 @@ function App() {
     var xp, s, result = 0, g = (x) / 10;
     for(var i = 0; i < 10; i++)
     {
-      xp = xa + g; //width of rectangle per iterations
-      s = sum(coordinatePairs.length) - sum(coordinatePairs.length-1);
+      s = sum(coordinatePairs.length) - sum(coordinatePairs.length-1); //finds the difference between two points
       result += s;
     }
 
@@ -48,67 +55,101 @@ function App() {
     
   }
 
+  var v = document.getElementById("content-box");
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function App() {
+
+  const [value,setValue] = useState(0);
+
+  const [balances, setBalances] = useState([]);
+
+  const saveValue = () => {
+      setBalances(prev => [...prev, value]);
+  }
+
+
+
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+  
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: balances,
+    }]
+  };
+  
+  const options = {};
+
+  
+
+  console.log(balances)
+
   return (
     <div className="bg">
 
       <header>
         Program by the Cafe Calligraphists
       </header>
-
       <body>
+      <script src="node_modules/chart.js/dist/Chart.bundle.min.js"></script>
+      <script src="App.js"></script>
+      <div>
+        <canvas id="myChart"></canvas>
+      </div>
 
       <h3 className="title">Money Mapper</h3>
 
         <div className="input-space">
 
           <label className="input-lbl" for="user-money">Current Balance: </label>
-          <input className="inputs" placeholder="i.e. 8000.25" type="text" id="user-money" name="user-money"/>
+          <input className="inputs" placeholder="i.e. 8000.25" type="text" id="user-money" name="user-money" value={value} onChange={(e) => setValue(e.target.value)}/>
               &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
               &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-          <input className="button" type="submit" id="money-submit" value="Submit" onclick="buttonPressed()"></input>
+          <button className="button" type="submit" id="money-submit" onClick={saveValue}></button>
 
         </div>
-
         <br/><br/><br/>
 
         <div className="input-space">
 
           <label className="input-lbl" for="user-spent">Amount Spent: </label> 
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <input className="inputs" placeholder="i.e. 8000.25" type="text" id="user-spent" name="user-spent"/>
+          <input className="inputs" placeholder="i.e. 8000.25" type="text" id="user-spent" name="user-spent" value={value} onChange={(e) => setValue(e.target.value)}/>
               &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
               &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-          <input className="button" type="submit" id="spent-submit" value="Submit" onclick="assignBalance()"></input>
+          <button className="button" type="submit" id="spent-submit" onClick={saveValue}></button>
 
         </div>
 
-        <div>
-          
-        </div>
+        <Line data={data}></Line>
 
         <br/>
+
+        
+
       </body>
     </div>  
   );
 }
-
-const data = {
-  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-  datasets: [
-    {
-      label: "First dataset",
-      data: [33, 53, 85, 41, 44, 65],
-      fill: true,
-      backgroundColor: "rgba(75,192,192,0.2)",
-      borderColor: "rgba(75,192,192,1)"
-    },
-    {
-      label: "Second dataset",
-      data: [33, 25, 35, 51, 54, 76],
-      fill: false,
-      borderColor: "#742774"
-    }
-  ]
-};
 
 export default App;
